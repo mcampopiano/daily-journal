8afbe5d75aaab7f3f5bfabe9a2b529bc3f3331e4
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-08 11:50:09 -0500

    created objects to represent journal entries and stored them in a new file, JournalDataProvider.js

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,44 @@
+/*
+ *   Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    }
+    {
+        id: 2,
+        date: "09/28/2020",
+        concept: "Terminal",
+        entry: "Today I learned about using an alias to declare a variable for the terminal, and echo to create a string.",
+        mood: "Ok"
+    }
+    {
+        id: 3,
+        date: "10/2/20",
+        concept: "github",
+        entry: "Started first group project. Learned how to push to github, approve and then pull each other's work.",
+        mood: "Ok"
+    }
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
