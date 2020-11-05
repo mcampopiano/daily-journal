c8cf0d2449761350f3e04edb66d906f623775c45
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-05 14:31:33 -0600

    created a mood provider module to get the moods from the json server

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,12 @@
 import { getEntries, useJournalEntries } from "./JournalDataProvider.js"
 import { RenderJournalEntries } from "./JournalEntryList.js"
 import { EntryForm } from "./JournalFormComponent.js"
+import { getMoods, useMoods } from "./MoodDataProvider.js"
 
 RenderJournalEntries()
 EntryForm()
+
+getMoods()
+.then(() => {
+    console.log("moods:", useMoods())
+})
